401f9952cb13618f80d134046008c8c13d59d22e
Mj Gaspar <gasparmj323@gmail.com>
2026-08-04 14:33:08 +0800

    Remove guest model and migration

--- database/migrations/2026_08_04_032353_create_staff_table.php
+++ database/migrations/2026_08_04_032353_create_staff_table.php
@@ -12,7 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('staff', function (Blueprint $table) {
-            $table->id();
+            $table->id('staff_id');
+
+            $table->foreignId('user_id')
+                  ->unique()
+                  ->constrained('users','id')
+                  ->cascadeOnDelete();
+
+            $table->foreignId('assigned_by')
+                  ->constrained('admins','admin_id')
+                  ->cascadeOnDelete();
+
+           
+
             $table->timestamps();
         });
     }
