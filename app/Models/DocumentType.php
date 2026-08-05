f03a3ca11c423c6d20be1adf64ff94cd9d24a29f
Mj Gaspar <gasparmj323@gmail.com>
2026-08-05 21:44:33 +0800

    Add models, migrations, seeders, and relationships

--- app/Models/DocumentType.php
+++ app/Models/DocumentType.php
@@ -7,10 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentType extends Model
 {
-    public function requests()
-{
-    return $this->hasMany(Request::class, 'document_type_id', 'document_type_id');
-}       
     /** @use HasFactory<\Database\Factories\DocumentTypeFactory> */
     use HasFactory;
+
+    protected $primaryKey = 'document_type_id';
+
+    protected $fillable = [
+        'document_name',
+        'description',
+        'processing_days',
+        'is_active',
+    ];
+
+    public function requests()
+    {
+        return $this->hasMany(Request::class, 'document_type_id', 'document_type_id');
+    }
 }
